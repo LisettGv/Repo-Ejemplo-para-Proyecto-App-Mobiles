cec4e4faef943895d9cab6e342c53eca8907ca0d
unknown <Cetecom@SJAPCA03LC0724.duoc.acad>
2025-10-01 11:55:48 -0300

    Repo Ejemplo para el Proyecto AppMobiles

---
+++ app/src/main/java/cl/mjl/myejemplo10/MainActivity.kt
@@ -0,0 +1,41 @@
+package cl.mjl.myejemplo10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.mjl.myejemplo10.ui.theme.Formulario
+import cl.mjl.myejemplo10.ui.theme.MyEjemplo10Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyEjemplo10Theme {
+                Formulario()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyEjemplo10Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/cl/mjl/myejemplo10/ui/Formulario.kt
@@ -0,0 +1,51 @@
+package cl.mjl.myejemplo10.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Formulario(){
+
+    var username by remember { mutableStateOf( "") }
+    var pass by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "Nombre")
+        OutlinedTextField(
+            value = username,
+            onValueChange = {
+                nuevotexto ->
+                username = nuevotexto
+            },
+            label = { Text(text = "Introduce tu nombre")}
+            )
+        Text(text = "Contraseña")
+        OutlinedTextField(
+            value = pass,
+            onValueChange = { nuevotexto ->
+                pass = nuevotexto
+            },
+            label = { Text(text = "Introduce tu contraseña")}
+        )
+        Button(onClick = { }) {
+            Text(text = "Enviar")
+        }
+    }
+
+}
