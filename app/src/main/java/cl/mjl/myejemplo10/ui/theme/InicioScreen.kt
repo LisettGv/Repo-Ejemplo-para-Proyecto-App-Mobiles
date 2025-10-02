fd87bbe411a2b342c599bd17565d8bf92564aa44
LisettGv <li.granadillo@duocuc.cl>
2025-10-02 15:09:53 -0300

    Codigo Completo!!

--- app/src/main/java/cl/mjl/myejemplo10/ui/theme/InicioScreen.kt
+++ app/src/main/java/cl/mjl/myejemplo10/ui/theme/InicioScreen.kt
@@ -4,24 +4,35 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+
+
 
 @Composable
 fun InicioScreen(){
 
-    Column(){
-        Text(text="Es Viernes !!!")
+    Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAligment = AlignmentLine.CenterHorizontaly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text(text="Es Viernes!!!")
         Text(text="Y tu cuerpo lo sabe")
         Text(text="Quiero comer y dormir")
 
-        Spacer(mofifier = Modifier.height(48.dp))
+        Spacer(modifier = Modifier.height(48.dp))
 
         Row{
-            Button(onClick = {  }) {Text("Ir al Fiesta")}
-            Spacer(mofifier = Modifier.width(16.dp))
-            Button(onClick = {  }) {Text("Ir a Mi Casa")}
+            Button(onClick = {  }) {
+                Text("Ir al Fiesta")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {  }) {
+                Text("Ir a Mi Casa")
+            }
         }
     }
 }
